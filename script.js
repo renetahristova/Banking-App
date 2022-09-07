455c322aab0390892d3134a9714f19a602430c8e
Reneta <renkagh55@gmail.com>
2022-09-07 20:11:52 +0300

    outs

--- script.js
+++ script.js
@@ -126,5 +126,9 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 };
 calcDisplaySummary(account1.movements);
