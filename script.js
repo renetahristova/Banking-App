9dc1d0c9f48f2ac846746666aa39fb3580ca69a9
Reneta <renkagh55@gmail.com>
2022-09-25 09:48:44 +0300

    loan functionality

--- script.js
+++ script.js
@@ -200,6 +200,17 @@ btnLogin.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //add movement
+    currentAccount.movements.push(amount);
+    //update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
